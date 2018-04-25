eca37a08171f17131bbf1db71210d4fbf2d74255
Christopher <cneil.villarroel@gmail.com>
2018-04-25 13:29:15 -0300

    Usuario y token listo

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 // Requires
 var express = require('express');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
 
 
 // Inicializar variables
 var app = express();
 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+
+
+
+// Importar rutas
+var appRoutes = require('./routes/app');
+var usuarioRoutes = require('./routes/usuario');
+var loginRoutes = require('./routes/login');
 
 // Conexión a la base de datos
 mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) => {
@@ -17,18 +29,13 @@ mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) =
 
 })
 
-
 // Rutas
-app.get('/', (req, res, next) => {
+app.use('/usuario', usuarioRoutes);
+app.use('/login', loginRoutes);
+app.use('/', appRoutes);
 
-    res.status(200).json({
-        ok: true,
-        mensaje: 'Peticion realizada correctamente'
-    })
-
-});
 
 // Escuchar peticiones
-app.listen(3000, () => {
-    console.log('Express server corriendo en el puerto 3000: \x1b[32m%s\x1b[0m', 'online');
+app.listen(3001, () => {
+    console.log('Express server corriendo en el puerto 3001: \x1b[32m%s\x1b[0m', 'online');
 });
